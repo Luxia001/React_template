a8d7ebfd9c898b21b4d7243fe686547a60e5cb01
Luxia <e14589654@gmail.com>
2025-02-23 23:02:30 +0700

    feat: :sparkles: material UI

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Switch } from "@mui/material";
 
 function Home() {
+  const [lang, setLang] = React.useState("en");
   return (
     <div>
       <h1>Home</h1>
@@ -8,6 +11,13 @@ function Home() {
       <p className="text-3xl font-bold text-blue-400 underline ">
         Tailwindcss 4.0
       </p>
+      <FormControlLabel
+        control={<Switch defaultChecked />}
+        label={lang}
+        onChange={(e) => {
+          setLang(e.target.checked ? "en" : "fr");
+        }}
+      />
     </div>
   );
 }
